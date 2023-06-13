520c8879e8cb54914fae2ad9ac537be9f07c6ca4
Luke Dixon <github@lukedixon.nl>
2023-06-13 13:07:03 +0200

    added yup and yum

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,27 @@
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import '../styles/ui/Auth/auth.css';
+
 const Login = () => {
-  const submitHandler = () => {};
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    // Perform login logic
+    console.log(values);
+    setSubmitting(false);
+  };
+
   return (
     <section className='container wrapper'>
       <section className='auth-container'>
@@ -9,31 +29,59 @@ const Login = () => {
         <div className='devider'>
           <span>Or</span>
         </div>
-        <form onSubmit={submitHandler}>
-          <fieldset>
-            <input
-              className='input'
-              type='email'
-              name='email'
-              id='email'
-              placeholder='Email Address'
-            />
-            <input
-              className='input'
-              type='password'
-              name='password'
-              id='password'
-              placeholder='Password'
-            />
-            <button type='submit' className='submit'>
-              Login
-            </button>
-          </fieldset>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form>
+              <fieldset>
+                <Field
+                  className={`input ${
+                    errors.email && touched.email ? 'error' : ''
+                  }`}
+                  type='email'
+                  name='email'
+                  id='email'
+                  placeholder='Email Address'
+                />
+                <ErrorMessage
+                  name='email'
+                  component='div'
+                  className='error'
+                  style={{ color: 'red' }}
+                />
+                <Field
+                  className={`input ${
+                    errors.password && touched.password ? 'error' : ''
+                  }`}
+                  type='password'
+                  name='password'
+                  id='password'
+                  placeholder='Password'
+                />
+                <ErrorMessage
+                  name='password'
+                  component='div'
+                  className='error'
+                  style={{ color: 'red' }}
+                />
+                <button
+                  type='submit'
+                  className='submit'
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? 'Logging in...' : 'Login'}
+                </button>
+              </fieldset>
+            </Form>
+          )}
+        </Formik>
         <button className='fg-password'>Forgot password</button>
         <hr />
 
-        <p className='small-copy'>Don't have a SnappCar account Yet?</p>
+        <p className='small-copy'>Don't have a SnappCar account yet?</p>
 
         <Link className='register' to='/register'>
           Register
