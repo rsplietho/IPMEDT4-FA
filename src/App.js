023b78236ca6a8ab3484e1f27d9f5e5b1fac88be
Luke Dixon <github@lukedixon.nl>
2023-06-13 19:51:17 +0200

    fixed alert and added login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import MobileNavBar from './components/navigation/MobileNavBar';
 import BottomNav from './components/navigation/BottomNav';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -8,6 +9,18 @@ function App() {
       <MobileNavBar />
       <Outlet />
       <BottomNav />
+      <ToastContainer
+        position='top-center'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
     </div>
   );
 }
