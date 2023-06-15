e3339c5dae0e881cc54ee5b3bb366cd14416141a
Luke Dixon <github@lukedixon.nl>
2023-06-15 14:44:15 +0200

    added links to detailed analytics

--- src/components/ui/home/MonthlyAnalytics.jsx
+++ src/components/ui/home/MonthlyAnalytics.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../../styles/ui/home/MonthlyAnalytics.css';
 import TipCard from '../../cards/TipCard';
 import AnalyticsCard from '../../cards/AnalyticsCard';
+import { Link } from 'react-router-dom';
 const MonthlyAnalytics = () => {
   return (
     <section
@@ -12,8 +13,12 @@ const MonthlyAnalytics = () => {
       <TipCard text='Want more insights? Click on the card' />
       <section className='analytics-grid'>
         {/*  prettier-ignore  */}
+        <Link to={'/income'}>
         <AnalyticsCard title='Income' value={1020.20} stat={10} />
-        <AnalyticsCard title='Reservations' value={20} stat={5} />
+        </Link>
+        <Link to={'/reservations'}>
+          <AnalyticsCard title='Reservations' value={20} stat={5} />
+        </Link>
         <AnalyticsCard title='Kilometers' value={500} stat={-2} />
         <AnalyticsCard title='Cancelations' value={0} stat={0} />
       </section>
